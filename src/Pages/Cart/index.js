1d71515ebfaae29a3eb71835caedc74400e48ae8
NaveenBantu <naveen.bantu1@gmail.com>
2022-09-17 19:26:42 +0200

    Title, favicon and styles updated

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -56,12 +56,12 @@ const Cart = () => {
                       />
                       <div className="flex flex-col ml-2 mt-2">
                         <Link to={`/product/${item.id}`}>
-                          <h2 className="text-sm title-font text-zinc-900 tracking-widest hover:text-blue-600 mt-2">
+                          <h2 className="text-sm title-font text-yellow-400 tracking-widest hover:text-blue-600 mt-2">
                             Brand
                           </h2>
                           <p className="font-extralight">{item.title}</p>
                         </Link>
-                        <p className="mt-auto mb-4 font-extralight text-xl">
+                        <p className="mt-auto mb-4 text-yellow-400 font-extralight text-xl">
                           $ {item.price}
                         </p>
                       </div>
